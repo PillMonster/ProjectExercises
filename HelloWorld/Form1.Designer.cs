270ca1bda7f38d112344cfe2181c5b4e3f446502
Dylan Chien <dylan9342@gmail.com>
2025-05-10 22:25:53 +0800

    chien

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labHelloWorld = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labCount = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timerCount = new System.Windows.Forms.Timer(this.components);
+            this.labTime = new System.Windows.Forms.Label();
+            this.timerTime = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labHelloWorld
+            // 
+            this.labHelloWorld.AutoSize = true;
+            this.labHelloWorld.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labHelloWorld.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labHelloWorld.Location = new System.Drawing.Point(71, 44);
+            this.labHelloWorld.Name = "labHelloWorld";
+            this.labHelloWorld.Size = new System.Drawing.Size(115, 16);
+            this.labHelloWorld.TabIndex = 0;
+            this.labHelloWorld.Text = "                           ";
+            this.labHelloWorld.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(64, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Say Hello Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labCount
+            // 
+            this.labCount.AutoSize = true;
+            this.labCount.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labCount.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labCount.Location = new System.Drawing.Point(285, 44);
+            this.labCount.Name = "labCount";
+            this.labCount.Size = new System.Drawing.Size(47, 16);
+            this.labCount.TabIndex = 2;
+            this.labCount.Text = "          ";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(252, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Count Button";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timerCount
+            // 
+            this.timerCount.Tick += new System.EventHandler(this.timerCount_Tick);
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.labTime.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labTime.Location = new System.Drawing.Point(410, 44);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(87, 16);
+            this.labTime.TabIndex = 4;
+            this.labTime.Text = "                    ";
+            // 
+            // timerTime
+            // 
+            this.timerTime.Tick += new System.EventHandler(this.timerTime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labCount);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labHelloWorld);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labHelloWorld;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labCount;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timerCount;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Timer timerTime;
+    }
+}
+
---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+
+        private int count;
+   
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerTime.Interval = 1000;
+            timerTime.Start(); // 啟動計時器
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labHelloWorld.Text = "Hello World!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            timerCount.Interval = 1000;
+            timerCount.Start(); // 啟動計時器
+    
+        }
+
+        private void timerCount_Tick(object sender, EventArgs e)
+        {
+
+            string str = count.ToString();
+            labCount.Text = str;
+
+            if (count == 10) { 
+                timerCount.Stop();
+            }
+            count++;
+
+        }
+
+        private void timerTime_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string formatted = now.ToString("yyyy/MM/dd HH:mm:ss");
+            labTime.Text = formatted;
+            //Console.WriteLine(formatted);
+        }
+    }
+}
